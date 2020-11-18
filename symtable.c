e947b1d5bfd25573706fec3bd4f5ce7975c59265
MrPrischep <57171271+MrPrischep@users.noreply.github.com>
2020-11-18 18:09:25 +0100

    Update symtable.c

--- symtable.c
+++ symtable.c
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include "symtable.h"
+#include "error.h"
 
-unsigned hash_code(char *key)
+unsigned hash_code(char *name)
 {
     unsigned hashval;
 
-    for (hashval = 0; *key != '\0'; key++)
-        hashval = key + 31 * hashval;
+    for (hashval = 0; *name != '\0'; name++)
+        hashval = name + 31 * hashval;
     return hashval % SYM_TABLE_SIZE;
 }
 
@@ -43,32 +44,65 @@ void free_sym_table(Sym_table *table) {
     }
 }
 
-void insert_item(Sym_table *table, char *key, Data data) {
-    Sym_table_item *item = search_item(table, key);
+int insert_item(Sym_table *table, Sym_table_item *node) {
+    Sym_table_item *item = search_item(table, node->name, node->depth);
 
     if (item != NULL) {
-        item->data = data;
-        return;
+        return SEMANTIC_UNDEFINED_VAR_ERROR;
     } else {
-        Sym_table_item *new_item = malloc(sizeof(struct Sym_table_item));
-
-        if (new_item == NULL) {
-            fprintf("Bad malloc\n");
-            return;
-        }
-        new_item->key = key;
-        new_item->data = data;
-        int index = hash_code(key);
+        Sym_table_item *new_item = node;        
+        int index = hash_code(node->name) + node->depth * 101;
         new_item->nextPtr = (*table)[index];
 
         (*table)[index] = new_item;
+        return 0;
     }
 
 }
 
+void delete_on_depth(Sym_table *table, char name, unsigned depth) {
+    int index = depth * 101;
+    Sym_table_item *item_for_delete = (*table)[index];
+    Sym_table_item *next_item = NULL;
 
-void delete_item(Sym_table *table, char key) {
-    int index = hash_code(key);
+    if (item_for_delete == NULL) {
+        return;
+    }
+    unsigned other_lvl = 5;
+    if (depth >= 5) {
+        for (unsigned i = other_lvl * INDEX_SIZE; i < SYM_TABLE_SIZE; i++) {
+            if ((*table)[i]->depth == depth) {
+
+                next_item = item_for_delete->nextPtr;
+
+                free(item_for_delete->name);
+                free(item_for_delete->value.func.in_var_list);
+                free(item_for_delete->value.func.out_var_list);
+                free(item_for_delete);
+
+                printf("Deleted index - %d\n", i);
+                item_for_delete = next_item;
+            }
+        }
+    } else {
+        for (unsigned i = depth * INDEX_SIZE; i < (depth + 1) * INDEX_SIZE; i++) {
+            if ((*table)[i] != NULL) {
+                next_item = item_for_delete->nextPtr;
+
+                free(item_for_delete->name);
+                free(item_for_delete->value.func.in_var_list);
+                free(item_for_delete->value.func.out_var_list);
+                free(item_for_delete);
+
+                printf("Deleted index - %d\n", i);
+                item_for_delete = next_item;
+            }
+        }
+    }
+}
+
+void delete_item(Sym_table *table, char name, unsigned depth) {
+    int index = hash_code(name);
     Sym_table_item *item_for_delete = (*table)[index];
 
     if (item_for_delete == NULL) {
@@ -81,7 +115,10 @@ void delete_item(Sym_table *table, char key) {
     while (item_for_delete != NULL) {
         next_item = item_for_delete->nextPtr;
 
-        if (strcmp(key, item_for_delete->key) == 0) {
+        if ((strcmp(name, item_for_delete->name) == 0) && (depth == item_for_delete->depth)) {
+            free(item_for_delete->name);
+            free(item_for_delete->value.func.in_var_list);
+            free(item_for_delete->value.func.out_var_list);
             free(item_for_delete);
 
             if (previous_item == NULL) {
@@ -98,19 +135,37 @@ void delete_item(Sym_table *table, char key) {
     }
 }
 
-Sym_table_item *search_item(Sym_table *table, char *key) {
-    if (table == NULL || key == NULL) {
+Sym_table_item *search_item(Sym_table *table, char *name, unsigned depth) {
+    if (table == NULL || name == NULL) {
         return NULL;
     }
 
-    int index = hash_code(key);
+    int index = hash_code(name);
     Sym_table_item *searching_item = (*table)[index];
 
     while (searching_item != NULL) {
-        if (strcmp(key, searching_item->key) == 0) {
+        if ((strcmp(name, searching_item->name) == 0) && (depth == searching_item->depth)) {
             return searching_item;
         }
 
         searching_item = searching_item->nextPtr;
     }
+}
+
+void htClearAll(Sym_table *table) {
+	Sym_table_item *item;
+	Sym_table_item *deleted_item;
+	for (int i = 0; i < SYM_TABLE_SIZE; i++) {
+		item = (*table)[i];
+
+		while (item != NULL) {
+			deleted_item = item;
+			item = deleted_item->nextPtr;
+            free(deleted_item->name);
+            free(deleted_item->value.func.in_var_list);
+            free(deleted_item->value.func.out_var_list);
+			free(deleted_item);
+		}
+		(*table)[i] = NULL;
+	}
 }
