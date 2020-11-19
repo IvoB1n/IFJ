b403f7c1182b7f1838c4e4ca7d412937f1a74132
IvoB1n <ivob1n@localhost.localdomain>
2020-11-19 21:19:21 +0100

    test_script: add reccurent search feature

--- tests/test_script.sh
+++ tests/test_script.sh
@@ -2,29 +2,53 @@
 
 # use ./tests/test_script.sh /tests/add/add (-l for less information) to run script
 
-dir_=$PWD;
 dir_to_search=$PWD$1;
+less_flag=$2
+
+search() {
+    for file in "$1"/*; do
+        if [ -f "$file" ]; then
+            test "$file"
+        elif [ -d "$file" ]; then
+        echo "  "
+        echo "  "
+        echo "  "
+        echo "----$file----"
+        echo "  "
+        
+            search "$file";
+        fi
+    done
+}
+
+test() {
+        if [ $less_flag = "-l" ]; then
+            echo "$1"
+            ./compiler < "$1" >/dev/null
+            ret_val=$?
+            line=$(head -n 1 $1)
+            echo "return value:      \e[0;36m $ret_val \e[0m   \e[0;36m ///// \e[0m"
+            echo "retvall:\e[0;33m $line \e[0m  \e[0;33m ///// \e[0m" 
+            echo "                        /"
+            echo "                       /"
+
+        else 
+            echo "$1"
+            ./compiler < "$1"
+            ret_val=$?
+            line=$(head -n 1 $1)
+            echo "return value:      \e[0;36m $ret_val \e[0m   \e[0;36m ///// \e[0m"
+            echo "expected:\e[0;33m $line \e[0m  \e[0;33m ///// \e[0m" 
+            echo "                        /"
+            echo "                       /"
+        fi
+        echo "  "
+}
 
 if [ $1 = "-h" ]; then
     echo "./tests/test_script.sh /tests/add/add (-l for less information)"
+elif [ -f $dir_to_search ]; then
+    test $dir_to_search
 else
-    for file in "$dir_to_search"/*; do
-        echo "   "
-        if [ $2 = "-l" ]; then
-            echo "$file"
-            ./compiler < "$file" >/dev/null
-            ret_val=$?
-            line=$(head -n 1 $file)
-            echo "return value: $ret_val"
-            echo  "expected: $line" 
-        else 
-            echo "$file"
-            ./compiler < "$file"
-            ret_val=$?
-            line=$(head -n 1 $file)
-            echo "return value: $ret_val"
-            echo  "expected: $line" 
-        fi
-        echo "  "
-    done
+    search "$dir_to_search"
 fi
